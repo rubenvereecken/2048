eff7538dcf509b981cb82a18f0867810bf3a707c
Ruben Vereecken <rubenvereecken@gmail.com>
2015-05-30 20:08:05 +0200

    completed the first iteration of the neural net algo

--- js/nnet-learner.js
+++ js/nnet-learner.js
@@ -22,6 +22,7 @@ NeuralNetLearner = (function(__super) {
 
     this.epsilon = 0.1;
     this.learnRate = 0.1;
+    this.gamma = 0.2;
   }
 
   return NeuralNetLearner;
@@ -34,9 +35,12 @@ var maybe = function(p) {
 
 NeuralNetLearner.prototype.reward = function() {
   // Base reward is difference between current score and previous score
+  // every merge thisis the value of the resulting tile
   var score = this.score - this.state.previousScore;
-  // Give an extra reward if a new highest tile has been reached
-  score += this.state.highestTile - this.grid.highestTile().value;
+  // Give an extra reward if a new highest tile has been reached, proportional to its value
+  var currentHighest = this.grid.highestTile().value;
+  if (currentHighest > this.state.highestTile)
+    score += this.state.highestTile;
   return score;
 };
 
@@ -67,16 +71,19 @@ NeuralNetLearner.prototype.think = function () {
   var reward;
   var move;
   var chosen;
+  var Q, maxQ;
+  var moveCandidate, input;
+
 
   // explore with epsilon chance
   if (maybe(this.epsilon)) {
     move = _.random(0, 3);
     chosen = this.input(move);
   } else {
-    var maxQ = 0;
-    for (var moveCandidate in [0, 1, 2, 3]) {
-      var input = this.input(moveCandidate);
-      var Q = this.network.activate(input);
+    maxQ = 0;
+    for (moveCandidate in [0, 1, 2, 3]) {
+      input = this.input(moveCandidate);
+      Q = this.network.activate(input);
       if (Q > maxQ) {
         chosen = input;
         maxQ = Q;
@@ -91,10 +98,23 @@ NeuralNetLearner.prototype.think = function () {
   this.state.totalReward += reward;
 
   // Update
+  // Find the highest new Q value Q(s', a')
+  maxQ = 0;
+  for (moveCandidate in [0, 1, 2, 3]) {
+    // this uses the new state we're currently in
+    input = this.input(moveCandidate);
+    Q = this.network.activate(input);
+    if (Q > maxQ) {
+      chosen = input;
+      maxQ = Q;
+      move = moveCandidate
+    }
+  }
+
   // do the move again so the neural net is prepared to backpropagate the value
-  // TODO wtf
-  this.activate(chosen);
-  this.propagate();
+  var oldQ = this.activate(chosen);
+  var newQ = oldQ + this.learnRate * (reward + this.gamma * maxQ - oldQ);
+  this.propagate(newQ);
 
   // finish up
   this.state.previousScore = this.score;
