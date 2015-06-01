947a95d570b147580d9a2bf9856518481e8dbe15
Ruben Vereecken <rubenvereecken@gmail.com>
2015-06-01 17:47:17 +0200

    restored nnet

--- js/application.js
+++ js/application.js
@@ -1,4 +1,4 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GreedyNeuralNet(4, new KeyboardInputManager(), new HTMLActuator(), new LocalStorageManager());
+  new NeuralNetLearner(4, new KeyboardInputManager(), new HTMLActuator(), new LocalStorageManager());
 });
